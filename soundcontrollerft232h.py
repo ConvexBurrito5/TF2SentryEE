6d25e5e6eae60de728ed9f4d6cb80641414de011
ConvexBurrito5 <33202798+ConvexBurrito5@users.noreply.github.com>
2024-07-30 17:51:15 -0400

    Fixed Sound files path

--- soundcontrollerft232h.py
+++ soundcontrollerft232h.py
@@ -7,10 +7,8 @@ class SoundControllerFT232H(SoundController):
     def __init__(self):
         current_file_path = os.path.abspath(__file__)
         current_dir = os.path.dirname(current_file_path)
-        parent_dir = os.path.dirname(current_dir)
-        self.sentry_scan_path = os.path.join(parent_dir, "SoundFiles", "Sentry_scan.wav")
-        self.sentry_spot_path = os.path.join(parent_dir, "SoundFiles", "Sentry_spot_client.wav")
-        pass
+        self.sentry_scan_path = os.path.join(current_dir, "SoundFiles", "Sentry_scan.wav")
+        self.sentry_spot_path = os.path.join(current_dir, "SoundFiles", "Sentry_spot_client.wav")
 
     def play_idle_beeping(self):
         playsound(self.sentry_scan_path)
