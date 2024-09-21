0203f2450ab93a87fd823271619e0e38d7e4cdef
Maiikiru <89322078+Maiikiru@users.noreply.github.com>
2024-09-21 13:42:18 -0400

    changed camera interface (#11)
    * changed camera interface
    
    * Added print info method

--- TurretSRC/FT232HCode/Camera/arducam.py
+++ TurretSRC/FT232HCode/Camera/arducam.py
@@ -1,19 +1,27 @@
 import cv2
 from IO.camera import Camera
 
-class DefaultCamera(Camera):
+
+'''
+Arducam Resolutions
+3264/2448	4:3     (15 fps)
+2592/1944	4:3     (15 fps)
+1920/1080	16:9    (30 fps)
+1600/1200	4:3     (30 fps)
+1280/720 	16:9    (30 fps)
+800/600		4:3     (30 fps)
+640/480		4:3     (30 fps)
+320/240 	4:3     (30 fps)
+'''
+class Arducam(Camera):
     def __init__(self, resolution=(640,480), cap_fps=30) -> None:
         super().__init__(resolution, cap_fps)
 
-        # Dont worry about resizing image because
-        # we're getting the correct res from the source.
-        self.cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+        self.cap = cv2.VideoCapture(0,cv2.CAP_V4L2)
+        self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, self.RESOLUTION[0])
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.RESOLUTION[1])
-        self.cap.set(cv2.CAP_PROP_FPS, self.CAPTURE_FPS)
-        # IT IS VERY VERY IMPORTANT THAT IT'S YU12
-        # the camera will give bad image if not configured this way.
-        self.cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('I', '4', '2', '0'))
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, self.RESOLUTION[1])
 
         self.print_info()
     
@@ -28,7 +36,7 @@ class DefaultCamera(Camera):
     def get_frame(self) -> cv2.typing.MatLike:
         """
         Returns the frame that the camera is currently reading.
-        returns img:MatLike row,col,BGR
+        returns img:MatLike row,col,BGR24 (8 bits per color)
         """
         ret_val, img = self.cap.read()
         if ret_val:
